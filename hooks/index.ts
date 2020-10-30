d3e76eb1f7f491001da377cbe1c9b86eda719d99
osequi <cs@osequi.com>
2020-10-30 18:51:05 +0200

    Importing useStyles

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
+export { useStyles } from "./use-styles/";
 export { useTheme } from "./use-theme";
 export { useColorContrast, useColorValue, useColorTune } from "./use-colors";
 export { useBreakpoint } from "./use-breakpoint";
