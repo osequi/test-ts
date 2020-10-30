77f3b1bedad475859fe10dc8d3545711f981ce28
osequi <cs@osequi.com>
2020-10-30 20:12:08 +0200

    Working on Text

--- components/typography/Text/Text.tsx
+++ components/typography/Text/Text.tsx
@@ -1,20 +1,45 @@
 import React, { FC, createElement } from "react";
 import { cx } from "emotion";
-import { useStyles, useTheme, useFindInArrays, useFont } from "../../../hooks";
 import { startCase } from "lodash";
 
+/**
+ * Imports other components ans hooks.
+ */
+import { useStyles, useTheme, useFindInArrays, useFont } from "../../../hooks";
+
+/**
+ * Defines the text types.
+ */
 export type TTextVariants = "default" | "body" | "longform" | "title";
 
+/**
+ * Defines the prop types.
+ */
 export type TText = {
   variant: TTextVariants;
-  as?: FC;
+  as?: FC | string;
   children?: any;
 };
 
+/**
+ * Defines the default props.
+ */
+const defaultProps: TText = {
+  variant: "default",
+  as: "div",
+  children: null,
+};
+
+/**
+ * Defines the styles.
+ */
 const defaultText = (props) => ({
   ...props.font,
 });
 
+/**
+ * Displays content inside a Text container.
+ */
 const Text = (props: TText) => {
   const { variant, as, children } = props;
 
@@ -30,7 +55,9 @@ const Text = (props: TText) => {
     className: cx(defaultTextKlass, `Text${startCase(variant)}`),
   };
 
-  return createElement("div", props2, children);
+  return createElement(as, props2, children);
 };
+
+Text.defaultProps = defaultProps;
 
 export default Text;
